d0873126f4acd9b6d61f45dc51c2203e4989a7fa
Vadim Suharnikov <vsuharnikov@gmail.com>
2026-02-28 10:41:26 +0400

    feat(inventory): re-export hotbar and equipment modules

--- src/inventory/mod.rs
+++ src/inventory/mod.rs
@@ -1,7 +1,11 @@
 pub mod components;
+pub mod equipment;
+pub mod hotbar;
 pub mod plugin;
 pub mod systems;
 
 pub use components::*;
+pub use equipment::*;
+pub use hotbar::*;
 pub use plugin::InventoryPlugin;
 pub use systems::*;
